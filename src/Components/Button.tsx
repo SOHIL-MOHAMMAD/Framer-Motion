a5e43063edeaea413b418579ed1a37085f362e72
sohil <sohil@mohammad>
2025-12-01 20:49:01 +0530

    button created

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import {motion} from "framer-motion"
 
 const Button = () => {
   return (
-    <div>
-      button
+    <div className="h-screen flex justify-center items-center bg-gray-950">
+     <motion.button
+     className="w-34 h-20 bg-blue-600 rounded-tr-3xl rounded-bl-3xl font-bold text-xl text-gray-200 rounded-tl-lg rounded-br-lg"
+     whileHover={{scale:1.1,rotate:1}}
+     whileTap={{scale:0.9}}>click here</motion.button>
     </div>
   )
 }
